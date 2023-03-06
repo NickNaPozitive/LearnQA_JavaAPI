20e6693e5cfa2814efbca94a44f0324232e25743
NickNaPoztive <20101999dN>
2023-03-06 22:54:38 +0300

    ex 11/fix test cookies

--- src/test/java/ex11.java
+++ src/test/java/ex11.java
@@ -1,5 +1,6 @@
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import lib.BaseTestCase;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
@@ -7,22 +8,24 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-public class ex11 {
+public class ex11 extends BaseTestCase {
     String url = "https://playground.learnqa.ru/api/homework_cookie";
-    String bad_url = "https://playground.learnqa.ru/api/homework_cookie2";
 
     @Test
-    public void goodTestCookie() {
+    public void goodTestHeader() {
         Response response = RestAssured
                 .get(url)
-//                .get(bad_url)
                 .andReturn();
         Map<String, String> cookies = response.getCookies();
 
         System.out.println(response.getCookies());
 
-        assertEquals(cookies, response.getCookies(), "Куки пусты");
+        Response response2 = RestAssured
+                .get(url)
+                .andReturn();
 
+        assertFalse(response2.getCookies().isEmpty(), "Куки пусты");
+        assertEquals(cookies, response2.getCookies(), "Куки не совпадают");
 
         }
 }
