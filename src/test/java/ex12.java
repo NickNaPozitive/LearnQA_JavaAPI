97846252031f07aebc3f7370a27e136b1a45484a
NickNaPoztive <20101999dN>
2023-03-06 19:06:28 +0300

    ex 13/fix assert

--- src/test/java/ex12.java
+++ src/test/java/ex12.java
@@ -1,12 +1,13 @@
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import lib.BaseTestCase;
 import org.junit.jupiter.api.Test;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ex12{
-    String url = "https://playground.learnqa.ru/api/homework_header";
+public class ex12 extends BaseTestCase {
+    String url = "https://playground.learnqa.ru/api/homework_header2";
 
     String searchingHeaderValue = "Some secret value";
     String searchingHeader= "x-secret-homework-header";
@@ -19,7 +20,7 @@ public class ex12{
 
         System.out.println(response.getHeaders());
 
-        assertEquals(searchingHeaderValue, response.getHeader( searchingHeader), "Header is`t found");
+        assertEquals(searchingHeaderValue, getHeader( response, searchingHeader));
 
 
     }
